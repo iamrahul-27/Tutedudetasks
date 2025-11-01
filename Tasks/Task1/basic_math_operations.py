20007aee2c0b8d6eee7394599e85861038fc0525
iamrahul-27 <prasadrahul6626@gmail.com>
2025-11-01 22:40:22 +0530

    Task 1 and Task 2 Completed Successfully

---
+++ Tasks/Task1/basic_math_operations.py
@@ -0,0 +1,8 @@
+var_1 = int(input("Enter First Number : "))
+var_2 = int(input("Enter First Number : "))
+
+print("\n\n")
+print("Addition: ",var_1+var_2)
+print("Subtraction: ",var_1-var_2)
+print("Multiplication: ",var_1*var_2)
+print("Division: ",var_1/var_2)
---
+++ Tasks/Task2/greeting.py
@@ -0,0 +1,5 @@
+first_name = input("ENter your first name: ")
+last_name = input("Enter your last name: ")
+full_name  = first_name + last_name
+
+print(f"\nHello, {full_name}! Welcome to the Python program.")
